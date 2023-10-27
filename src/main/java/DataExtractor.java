9d0220f59fe095ab6c7d3679981eb4b86bb080fe
Abd-404 <abdullahmeraj243@gmail.com>
2023-10-27 18:07:40 +0300

    added extraciton logic

--- src/main/java/DataExtractor.java
+++ src/main/java/DataExtractor.java
@@ -1,7 +1,132 @@
 // apache library imports
-
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.util.CellAddress;
+import org.apache.poi.xssf.usermodel.*;
 // necessary java imports
-
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 public class DataExtractor {
-    // extraction logic
+    public static void extractData(){
+        LinkedHashMap<String, Object> jsonMap = new LinkedHashMap<>();
+        String courseName = "courseName";
+        List<String> daysList = List.of("sunday", "monday", "tuesday", "wednesday", "thursday");
+        String excelFilePath = "src/main/resources/ROOMz.20231.xlsx";
+        FileInputStream inputStream = Utils.getExcelFile(excelFilePath);
+        XSSFWorkbook workbook = Utils.getExcelWorkbook(inputStream);
+        if(inputStream == null || workbook == null) {
+            return;
+        }
+
+
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        CellAddress firstRoomCell = new CellAddress("Y4");
+        int roomCol = firstRoomCell.getColumn();
+        int lastRow = sheet.getLastRowNum();
+
+        // loop over all the rooms
+        for (int currRow = 3; currRow < lastRow; currRow++) {
+            XSSFRow roomRow = sheet.getRow(currRow);
+            if (roomRow.getLastCellNum() <= 1) {
+                continue;
+            }
+            XSSFCell roomCell = roomRow.getCell(roomCol);
+            if (roomCell.getCellType() != CellType.STRING || roomCell.getStringCellValue().equals("ONLINE")) {
+                continue;
+            }
+
+            LinkedHashMap<String, Object> innerMap = new LinkedHashMap<>();
+            innerMap.put("name", roomCell.getStringCellValue());
+            // initializing each day as null the semicolons are used to format it into json
+            // map
+            daysList.forEach(day -> innerMap.put(day, ";null;"));
+            XSSFRow daysRow = sheet.getRow(currRow + 1);
+
+            // check which days are busy in the particular room
+            for (int j = 9; j < daysRow.getLastCellNum(); j++) {
+                ArrayList<LinkedHashMap<String, Object>> dayCourses = new ArrayList<>();
+                XSSFCell daysCell = daysRow.getCell(j);
+                if (daysCell.getCellType() != CellType.STRING) {
+                    continue;
+                }
+                String dayCellString = daysCell.getStringCellValue().toLowerCase();
+
+                for (int coursesRowNum = daysRow.getRowNum() + 2;; coursesRowNum++) {
+                    LinkedHashMap<String, Object> courseDetailsMap = new LinkedHashMap<>();
+                    XSSFRow coursesRow = sheet.getRow(coursesRowNum);
+                    if (coursesRow == null || coursesRow.getLastCellNum() <= 1) {
+                        break;
+                    }
+                    XSSFCell courseCell = coursesRow.getCell(j);
+                    if (courseCell.getCellType() != CellType.STRING
+                            || coursesRow.getCell(0).getCellType() == CellType.BLANK) {
+                        continue;
+                    }
+                    String courseTimeStart = coursesRow.getCell(0).getStringCellValue();
+
+
+                    LinkedHashMap<String, String> startTimesMap = Utils.formatToMap(courseTimeStart);
+
+                    String courseTimeEnd = coursesRow.getCell(5).getStringCellValue();
+
+                    LinkedHashMap<String, String> endTimesMap = Utils.formatToMap(courseTimeEnd);
+
+                    courseDetailsMap.put("timeStart", startTimesMap);
+                    courseDetailsMap.put("timeEnd", endTimesMap);
+                    courseDetailsMap.put(courseName, courseCell.getStringCellValue());
+                    if (!dayCourses.isEmpty()) {
+                        LinkedHashMap<String, Object> freeTimeMap = new LinkedHashMap<>();
+                        LinkedHashMap<String, Object> prevEntry = dayCourses.get(dayCourses.size() - 1);
+                        LinkedHashMap<String, String> prevTimeEnd = (LinkedHashMap<String, String>) prevEntry
+                                .get("timeEnd");
+                        freeTimeMap.put("timeStart", prevTimeEnd);
+                        freeTimeMap.put("timeEnd", startTimesMap);
+                        freeTimeMap.put("courseName", "Free");
+                        dayCourses.add(freeTimeMap);
+                    }
+                    dayCourses.add(courseDetailsMap);
+
+                }
+                innerMap.put(dayCellString, dayCourses);
+            }
+            jsonMap.put(roomCell.getStringCellValue(), innerMap);
+
+        }
+        Utils.closeWorkBook(workbook);
+        // converting java map to json map
+        String rawMapString = jsonMap.toString().replace("=", ":").replace(Character.toString(160), " ");
+        StringBuilder jsonMapString;
+        jsonMapString = new StringBuilder();
+        char[] rawMapCharArray = rawMapString.toCharArray();
+        boolean isQuoteOpen = false;
+        boolean isPaused = false;
+        for (char character : rawMapCharArray) {
+            if (!Character.isLetterOrDigit(character)) {
+                if (character == ';') {
+                    isPaused = !isPaused;
+                    continue;
+                }
+                if (isQuoteOpen && !Character.isWhitespace(character) && character != '-' && character != '_') {
+                    isQuoteOpen = false;
+                    jsonMapString.append('"');
+                }
+                jsonMapString.append(character);
+                continue;
+            }
+            if (!isQuoteOpen && !isPaused) {
+                jsonMapString.append('"');
+                isQuoteOpen = true;
+            }
+            jsonMapString.append(character);
+
+        }
+        Utils.saveStrToFile(jsonMapString.toString());
+    }
+
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("cool stuff are coming <3");
+        DataExtractor.extractData();
     }
 }
